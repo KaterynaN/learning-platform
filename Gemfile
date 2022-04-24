c028dcaedef980ec642dad2bad951ccb9c1853bc
KaterynaN <kateryna.od.ua@gmail.com>
2022-04-25 00:16:01 +0300

    add user authentication with devise

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem "haml-rails", "~> 2.0"
 gem 'simple_form'
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
-
+gem 'devise'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :courses
-  root 'courses#index'
-
+  get 'static_pages/langing_page'
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
