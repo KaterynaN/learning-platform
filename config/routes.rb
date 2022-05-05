1f7496f13163d692726fd394c373bddc9a31886b
KaterynaN <kateryna.od.ua@gmail.com>
2022-05-05 17:33:35 +0300

    Add trackable for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   get 'static_pages/langing_page'
   root 'static_pages#landing_page'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
