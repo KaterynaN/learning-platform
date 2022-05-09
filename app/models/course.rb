0a08ea6033d4f63a206825615971d0b2446133c9
KaterynaN <kateryna.od.ua@gmail.com>
2022-05-09 20:53:32 +0300

    add new fields to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
 
-  validates :title, presence: true
+  validates :title, :level, :language, :price, presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   has_rich_text :description
 
